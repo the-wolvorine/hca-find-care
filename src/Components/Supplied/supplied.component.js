d1ea5b4b3c0542d9fd1700dff55de8a96f02bf96
the-wolvorine <uday.cheekatipalli@slu.edu>
2024-08-08 11:48:30 -0500

    Updated supplied component to show the term list based on the filtered letter.

--- src/Components/Supplied/supplied.component.js
+++ src/Components/Supplied/supplied.component.js
@@ -1,16 +1,46 @@
-
+import { useEffect, useState } from "react";
+import data from "./data.json";
 
 const SuppliedComponent = () => {
-    return (
+    const [selectedLetter, setSelectedLetter] = useState('');
+    const [filteredTerms, setFilteredTerms] = useState(data.termList);
+
+    useEffect(() => {
+        const filtered = data.termList.filter(term => term.title.startsWith(selectedLetter));
+        setFilteredTerms(filtered);
+    }, [selectedLetter]);
+
+    const handleLetterClick = (letter) => {
+        setSelectedLetter(letter);
+    }
+
+    const showTerms = () => {
+        return filteredTerms.map((term, index) => (
+            <div key={term.index}>
+                <a href={term.link}>{term.title}</a>
+            </div>
+        ));
+    }
+
+    const showLetters = () => {
+        const allLetters = Array.from({length: 26}, (_,i) => String.fromCharCode(i+65));
+        const existingLetters = new Set(data.termList.map(term => term.title[0].toUpperCase()));
+        return allLetters.map((letter, index) => (
+            <button key={index} onClick={() => handleLetterClick(letter)} disabled={!existingLetters.has(letter)} >
+                {letter}
+            </button>
+        ));
+    }
+    return(
         <div className="container">
-        <div className="letter-buttons">
-            Show Letters
-        </div>
-        <div className="term-list">
-            Show Terms List
-        </div>
+            <div className="letter-buttons">
+                {showLetters()}
+            </div>
+            <div className="terms-list">
+                {showTerms()}
+            </div>
         </div>
     );
-};
+}
 
 export default SuppliedComponent;
