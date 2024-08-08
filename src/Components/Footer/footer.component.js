af222dd91cb8dcc7d0fce64a113cf77a3759ee2c
the-wolvorine <uday.cheekatipalli@slu.edu>
2024-08-08 12:48:08 -0500

    Updated Header and Footer Components.

--- src/Components/Footer/footer.component.js
+++ src/Components/Footer/footer.component.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import Logo from "./HCA_Footer_Logo.png";
+import "./footer.css";
+
+
 const Footer = () => {
-    return(
-        <div>
-            <p>HCA - Houston Health Care</p>
-        </div>
-    );
-}
+  return (
+    <footer className="footer">
+      <div className="logo">
+        <img src={Logo} alt="HCA+ Houston Healthcare" />
+      </div>
+      <div className="contact">
+        <p>3737 Buffalo Speedway</p>
+        <p>Suite 1400</p>
+        <p>Houston, TX 77098</p>
+        <a href="#">Phone Directory</a>
+        <a href="#">Maps & Directions</a>
+        <a href="#">Media Center</a>
+        <a href="#">Contact Us</a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
